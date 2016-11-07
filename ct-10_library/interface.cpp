e29bf93dc1daebf1a8a000c8ccbed020c137edd1
josh <josh@DESKTOP-3KTUBOJ>
2016-11-07 19:14:31 +0900

    Initial

---
+++ ct-10_library/interface.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   interface.cpp
+ * Author: josh
+ * 
+ * Created on November 6, 2016, 10:44 PM
+ */
+
+#include "interface.h"
+
+interface::interface() {
+}
+
+interface::interface(const interface& orig) {
+}
+
+interface::~interface() {
+}
+
