7e5fee8475ad5b79248b654a8bebbdcda5a9aa76
josh <josh@impson.us>
2016-11-07 20:34:14 +0900

    Initial

---
+++ ct10/reg.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   reg.cpp
+ * Author: josh
+ * 
+ * Created on November 7, 2016, 8:34 PM
+ */
+
+#include "reg.h"
+
+reg::reg() {
+}
+
+reg::reg(const reg& orig) {
+}
+
+reg::~reg() {
+}
+
